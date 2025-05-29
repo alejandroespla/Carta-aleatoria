d82d066cac3c6659aa14bc6ea9f53b9189af2746
Espla <37451532+alejandroespla@users.noreply.github.com>
2025-05-29 06:39:42 +0000

    cartas

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import "bootstrap";
 import "./style.css";
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+ let palos = ['♦', '♥', '♠', '♣'];
+ let numeros = ['A', '2' , '3', '4', '5', '6', '7', '8', '9', 'J', 'Q', 'K']
+ 
+ let paloIndex = Math.floor(Math.random() * palos.length);
+ let numeroIndex = Math.floor(Math.random() * numeros.length);
+
+ let cardPalo = palos[paloIndex];
+ let cardNumero = numeros[numeroIndex];
+
+let color = (cardPalo === '♠' || cardPalo === '♣') ? 'black' : 'red';
+
+ document.querySelectorAll('.palo').innerText = cardPalo;
+  document.querySelectorAll('.palo').forEach((el) => {
+    el.innerText = cardPalo;
+    el.style.color = color;
+  });
+
+ document.querySelector('.number').innerText = cardNumero;
+
 };
+
